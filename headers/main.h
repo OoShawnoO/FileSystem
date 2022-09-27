88b82d169b8f4b7fc97cfc0ab27fa5c91cb83599
OoShawnoO <8408031038@qq.com>
2022-09-27 16:00:09 +0800

    2022-9-27 from ubuntu

--- headers/main.h
+++ headers/main.h
@@ -9,59 +9,14 @@ string cmd;
 vector<string> params;
 vector<user_c*> users;
 
-user_c* user = new user_c("root");
+user_c* user;
+dir_c* root;
 
-dir_c* root = new dir_c(user,"/",NULL);
 
+void split(const string& str,char splitchar,vector<string>& res);
 
-void split(const string& str,char splitchar,vector<string>& res){
-    string strs = str + splitchar;
-    int pos = strs.find(splitchar);
-    while(pos != string::npos){
-        string temp = strs.substr(0,pos);
-        res.push_back(temp);
-        strs = strs.substr(pos+1);
-        pos = strs.find(splitchar);
-    }
-}
+void color_cout(int layout,int color,string s);
 
-void color_cout(int layout,int color,string s){
-    cout << "\e["<< layout << ";" << color << "m" << s << "\e[0m";
-}
-
-void uerror(ERROR e){
-    switch (e)
-    {
-        case PERMISSION : {
-            color_cout(HIGHTLIGHT,F_RED,"ERROR : Permission denied .");
-            cout << endl;
-            break;
-        }
-
-        case NOTDIR : {
-            color_cout(HIGHTLIGHT,F_RED,"ERROR : Its not a dir .");
-            cout << endl;
-            break;
-        }
-        
-        case NOTFOUND : {
-            color_cout(HIGHTLIGHT,F_RED,"ERROR : File not found .");
-            cout << endl;
-            break;
-        }
-        
-        case CMD : {
-            color_cout(HIGHTLIGHT,F_RED,"ERROR : Unknown command .");
-            cout << endl;
-            break;
-        }
-
-        default : {
-            break;
-        }
-
-    }
-    
-}
+void uerror(ERROR e);
 
 #endif
--- sources/main.cpp
+++ sources/main.cpp
@@ -9,8 +9,61 @@
 //完成系统的 总控模块编程和各个操作模块编程。上机测试各个模块，没执行一个操作，打印有关数据结构的内容
 //判断操作的正确性。
 
+void split(const string& str,char splitchar,vector<string>& res){
+    string strs = str + splitchar;
+    int pos = strs.find(splitchar);
+    while(pos != string::npos){
+        string temp = strs.substr(0,pos);
+        res.push_back(temp);
+        strs = strs.substr(pos+1);
+        pos = strs.find(splitchar);
+    }
+}
+
+void color_cout(int layout,int color,string s){
+    cout << "\e["<< layout << ";" << color << "m" << s << "\e[0m";
+}
+
+void uerror(ERROR e){
+    switch (e)
+    {
+        case PERMISSION : {
+            color_cout(HIGHTLIGHT,F_RED,"ERROR : Permission denied .");
+            cout << endl;
+            break;
+        }
+
+        case NOTDIR : {
+            color_cout(HIGHTLIGHT,F_RED,"ERROR : Its not a dir .");
+            cout << endl;
+            break;
+        }
+        
+        case NOTFOUND : {
+            color_cout(HIGHTLIGHT,F_RED,"ERROR : File not found .");
+            cout << endl;
+            break;
+        }
+        
+        case CMD : {
+            color_cout(HIGHTLIGHT,F_RED,"ERROR : Unknown command .");
+            cout << endl;
+            break;
+        }
+
+        default : {
+            break;
+        }
+
+    }
+    
+}
+
 int main()
 {
+    user = new user_c("root");
+
+    root = new dir_c(user,"/",NULL);
 
     user->set_current_dir(root);
 
